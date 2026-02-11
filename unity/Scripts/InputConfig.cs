15bd8eddefc64e1369cbff7dd265f59736ca2528
jesus-justin <mjesusjustin@gmail.com>
2026-02-11 19:54:58 +0800

    unity: add input config asset

---
+++ unity/Scripts/InputConfig.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace CampusNavigator
+{
+    [CreateAssetMenu(menuName = "CampusNavigator/InputConfig")]
+    public class InputConfig : ScriptableObject
+    {
+        public KeyCode interactKey = KeyCode.E;
+        public KeyCode submitKey = KeyCode.F;
+        public KeyCode inventoryKey = KeyCode.I;
+        public KeyCode questListKey = KeyCode.Q;
+
+        public KeyCode dialogueChoice1 = KeyCode.Alpha1;
+        public KeyCode dialogueChoice2 = KeyCode.Alpha2;
+        public KeyCode dialogueChoice3 = KeyCode.Alpha3;
+    }
+}

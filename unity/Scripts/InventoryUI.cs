066bb5116848d3100aa7d75bbb3f9f995f87aec9
jesus-justin <mjesusjustin@gmail.com>
2026-02-11 20:38:23 +0800

    unity: add inventory UI

---
+++ unity/Scripts/InventoryUI.cs
@@ -0,0 +1,59 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CampusNavigator
+{
+    public class InventoryUI : MonoBehaviour
+    {
+        public static InventoryUI Instance { get; private set; }
+
+        public GameObject panel;
+        public Text listText;
+        public InputConfig inputConfig;
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            Instance = this;
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(GetToggleKey()))
+            {
+                Toggle();
+            }
+        }
+
+        public void Toggle()
+        {
+            if (panel == null)
+            {
+                return;
+            }
+            panel.SetActive(!panel.activeSelf);
+            Refresh(InventoryManager.Instance?.GetItems() ?? new string[0]);
+        }
+
+        public void Refresh(IEnumerable<string> items)
+        {
+            if (listText == null)
+            {
+                return;
+            }
+            var list = items != null ? items.ToList() : new List<string>();
+            listText.text = list.Count == 0 ? "Inventory empty" : string.Join("\n", list);
+        }
+
+        private KeyCode GetToggleKey()
+        {
+            return inputConfig != null ? inputConfig.inventoryKey : KeyCode.I;
+        }
+    }
+}

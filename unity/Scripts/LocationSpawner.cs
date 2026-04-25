3f155b5e2ac24f7a697d6a0d8140d99801f718f0
jesus-justin <mjesusjustin@gmail.com>
2026-04-25 21:16:40 +0800

    feat(unity): integrate location spawner with campus anchors

--- unity/Scripts/LocationSpawner.cs
+++ unity/Scripts/LocationSpawner.cs
@@ -1,17 +1,32 @@
 using System.Collections;
+using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 namespace CampusNavigator
 {
     public class LocationSpawner : MonoBehaviour
     {
+        public event Action<LocationDto[]> LocationsLoaded;
+
         public GameObject markerPrefab;
         public bool autoLoginIfMissing = true;
         public string autoLoginExternalId = "student-001";
         public string autoLoginName = "Student";
+        public CampusMapRuntime mapRuntime;
+        public bool snapToCampusAnchors = true;
+        public bool hideMarkerRendererWhenAnchored = true;
+        public bool logCoverageOnLoad = true;
+
+        private readonly HashSet<int> mappedIds = new HashSet<int>();
+        private int totalLoaded;
 
         private void Start()
         {
+            if (mapRuntime == null)
+            {
+                mapRuntime = CampusMapRuntime.Instance;
+            }
             StartCoroutine(LoadLocations());
         }
 
@@ -42,9 +57,20 @@ namespace CampusNavigator
                 return;
             }
 
+            LocationsLoaded?.Invoke(res.locations);
+
+            mappedIds.Clear();
+            totalLoaded = res.locations.Length;
+
             foreach (var loc in res.locations)
             {
                 SpawnMarker(loc);
+            }
+
+            if (logCoverageOnLoad)
+            {
+                int covered = mappedIds.Count;
+                Debug.Log("LocationSpawner: loaded " + totalLoaded + " API locations. Anchored to 3D model: " + covered + ".");
             }
         }
 
@@ -68,7 +94,18 @@ namespace CampusNavigator
             }
 
             marker.name = "Location_" + loc.id + "_" + loc.name;
-            marker.transform.position = new Vector3(loc.pos_x, loc.pos_y, loc.pos_z);
+
+            bool hasAnchor = false;
+            if (snapToCampusAnchors && mapRuntime != null && mapRuntime.TryGetAnchor(loc.id, out var anchor) && anchor != null)
+            {
+                marker.transform.position = anchor.position;
+                hasAnchor = true;
+                mappedIds.Add(loc.id);
+            }
+            else
+            {
+                marker.transform.position = new Vector3(loc.pos_x, loc.pos_y, loc.pos_z);
+            }
 
             var markerComp = marker.GetComponent<LocationMarker>();
             if (markerComp == null)
@@ -81,7 +118,24 @@ namespace CampusNavigator
             LocationRegistry.Register(loc.id, marker.transform, loc.name);
 
             float radius = Mathf.Max(1f, loc.radius_m);
+            if (mapRuntime != null)
+            {
+                float overrideRadius = mapRuntime.GetRadiusOverride(loc.id);
+                if (overrideRadius > 0f)
+                {
+                    radius = overrideRadius;
+                }
+            }
             marker.transform.localScale = new Vector3(radius * 2f, marker.transform.localScale.y, radius * 2f);
+
+            if (hasAnchor && hideMarkerRendererWhenAnchored)
+            {
+                var renderers = marker.GetComponentsInChildren<Renderer>(true);
+                for (int i = 0; i < renderers.Length; i++)
+                {
+                    renderers[i].enabled = false;
+                }
+            }
         }
     }
 }
